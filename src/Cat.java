33345f3565223889287f60bfdda02ca14690e7e4
ZhaoZMM <zhaoz@nycstudents.net>
2023-10-25 09:02:59 -0400

    a

---
+++ src/Cat.java
@@ -0,0 +1,42 @@
+
+public class Cat
+{
+    private String name;
+    private int age;
+
+    public Cat(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    // add a getAge() getter below for
+    // obtaining the Cat's age
+    public int getAge(){
+        return age;
+    }
+
+
+    // MODIFY this method so that the Cat randomly
+    // returns one of three different messages
+    // (make any messages you want!) rather than always returning "meow".
+    // Each message should have equal chance of being returned.
+    public String speak() {
+        int rand1 = (int) (Math.random()*2+1);
+        if (rand1==1){
+            return "meow";
+        }
+        if (rand1==2){
+            return "wow";
+        }
+        return "zhao";
+    }
+
+    public void introduce() {
+        System.out.println("Hello. My name is " + name + " and I am " + age + " years old. Now, mind your own business.");
+    }
+}
